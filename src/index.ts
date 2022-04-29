ad6bcf8787f84a26821e162282086d95fc33c55b
Kevin Liao <q85292542000@gmail.com>
2022-04-29 22:21:15 +0800

    style(src/main.ts): change styling.

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,22 @@
-import express from 'express';
-import { userRouter } from './routes/api';
-import { createConnection } from 'typeorm';
-import 'reflect-metadata';
-import bodyParser from 'body-parser';
+import express from "express";
+import { userRouter } from "./routes/api";
+import { createConnection } from "typeorm";
+import "reflect-metadata";
+import bodyParser from "body-parser";
+import { initDatabase } from "./database/databaseInit";
 
 const server = async () => {
   const app = express();
   await createConnection();
+  await initDatabase();
 
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
 
-  app.use('/user', userRouter);
+  app.use("/user", userRouter);
 
-  app.listen({ port: 3000, hostname: '0.0.0.0' }, () => {
-    console.log('server started');
+  app.listen({ port: 3000, hostname: "0.0.0.0" }, () => {
+    console.log("Server start at port 3000");
   });
 };
 
